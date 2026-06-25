5d0ddc793900104e81e39f9e05e414d10b806138
Moritz Zwerger <bixilon@bixilon.de>
2026-06-25 12:13:28 +0200

    entity mesh feature: don't try to unload mesh multiple times
    It is automatically enqueued for unloading when calling `this.mesh = null`.
    
    ```
    java.lang.IllegalStateException: Vertex buffer is not uploaded: UNLOADED
      at de.bixilon.minosoft.gui.rendering.system.opengl.buffer.vertex.OpenGlVertexBuffer.unload(OpenGlVertexBuffer.kt:104)
      at de.bixilon.minosoft.gui.rendering.util.mesh.Mesh.unload(Mesh.kt:33)
      at de.bixilon.minosoft.gui.rendering.entities.feature.mesh.MeshedFeature.enqueueUnload$lambda$0(MeshedFeature.kt:48)
      at de.bixilon.kutil.concurrent.queue.Queue.work(Queue.kt:46)
      at de.bixilon.kutil.concurrent.queue.Queue.work$default(Queue.kt:36)
      at de.bixilon.minosoft.gui.rendering.entities.EntitiesRenderer.postPrepareDraw(EntitiesRenderer.kt:83)
      at de.bixilon.minosoft.gui.rendering.renderer.renderer.RendererManager.prepare(RendererManager.kt:116)
      at de.bixilon.minosoft.gui.rendering.renderer.renderer.RendererManager.draw(RendererManager.kt:122)
      at de.bixilon.minosoft.gui.RenderLoop.loop(RenderLoop.kt:75)
      at de.bixilon.minosoft.gui.RenderLoop.startLoop(RenderLoop.kt:131)
      at de.bixilon.minosoft.gui.rendering.Rendering.startRenderWindow(Rendering.kt:79)
      at de.bixilon.minosoft.gui.rendering.Rendering.startRenderWindowThread$lambda$0(Rendering.kt:68)
      at java.base/java.lang.Thread.run(Thread.java:1583)
      ```
    
    Fixes GH-44

--- src/main/java/de/bixilon/minosoft/gui/rendering/entities/feature/mesh/MeshedFeature.kt
+++ src/main/java/de/bixilon/minosoft/gui/rendering/entities/feature/mesh/MeshedFeature.kt
@@ -1,6 +1,6 @@
 /*
  * Minosoft
- * Copyright (C) 2020-2025 Moritz Zwerger
+ * Copyright (C) 2020-2026 Moritz Zwerger
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -52,10 +52,7 @@ abstract class MeshedFeature<M : Mesh>(
     override fun enqueueUnload() {
         super.enqueueUnload()
         if (unload) {
-            val mesh = this.mesh ?: return
-            enqueueUnload(mesh)
             this.mesh = null
-            this.unload = false
         }
     }
 
